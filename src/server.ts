f85936c5d2e9bcc87927b492b010ca097ae045e6
Gabriel Leao <gabriel.leao@catskillet.com>
2023-03-21 10:38:00 -0300

    feat: cors adicionado

--- src/server.ts
+++ src/server.ts
@@ -2,12 +2,14 @@ import bodyParser from 'body-parser'
 import express from 'express'
 import { dataSource } from './data-source'
 import { routes } from './routes/index.routes'
+import cors from 'cors'
 
 dataSource
   .initialize()
   .then(() => {
     const port = process.env.PORT || 3333
     const app = express()
+    app.use(cors())
     app.use(bodyParser.urlencoded({ extended: false }))
     app.use(bodyParser.json())
     app.use(routes)
