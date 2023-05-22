09b63d43a80b33f4683379e0c314b9013f662b55
Gabriel-Leao <gabriel.leao2507@gmail.com>
2023-05-22 12:36:32 -0300

    feat: remove vercel file

--- src/server.ts
+++ src/server.ts
@@ -3,13 +3,12 @@ import express from 'express'
 import { routes } from './routes/index.routes'
 import cors from 'cors'
 import { database } from './config/db.config'
-export let app: any
 
 database
   .authenticate()
   .then(() => {
     const port = process.env.PORT || 3333
-    app = express()
+    const app = express()
     app.use(cors())
     app.use(bodyParser.urlencoded({ extended: false }))
     app.use(bodyParser.json())
